0a2c076e71433e52b76aa49d4b17bb3af35c8ee5
riv2q <rozycki.krystian@gmail.com>
2016-08-31 19:15:02 +0200

    adds some to bin method

--- build-in/functions/_bin.py
+++ build-in/functions/_bin.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import random
 
 __doc__ = """bin(x)
@@ -8,21 +9,38 @@ New in version 2.6.
 """
 
 
+class memorize(dict):
+    """cache decorator
+    """
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self, *args):
+        return self[args]
+
+    def __missing__(self, key):
+        result = self[key] = self.function(*key)
+        return result
+
+
 def sample():
     """sample of `bin` usage
     """
     numbers = [random.randint(0, 100) for x in xrange(10)]
-    print 'result: {result}'.format(result='OK' if all(
-        [bin(n) == convert_to_binary(n) for n in numbers]) else 'BLAD')
+    map(lambda x: print(bin(x), ' == ', convert_to_binary(x)), numbers)
+    print('result: {result}'.format(result='OK' if all(
+        [bin(n) == convert_to_binary(n) for n in numbers]) else 'BLAD'))
 
+
+@memorize
 def convert_to_binary(number):
     """converts number to binary output
     """
     binary_tab = []
-    while number >= 0:
+    while number:
         binary_tab.insert(0, number % 2)
         number //= 2
     return '0b{binary}'.format(
-        binary=''.join(map(str, binary_tab)))
+        binary=''.join(map(str, binary_tab or [0])))
 
 sample()
